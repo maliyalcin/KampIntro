35d616b4f9792840cf6482ed99bc968fe427a781
maliyalcin08 <maliyalcin08@gmail.com>
2021-02-21 17:14:32 +0300

    Dictionary

---
+++ Dictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary <string,int> ilPlaka = new Dictionary<string,int>();
+
+            ilPlaka.Add("Artvin",08);
+            ilPlaka.Add("İstanbul",34);
+            ilPlaka.Add("Ankara",06);
+
+            foreach (var item in ilPlaka)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
