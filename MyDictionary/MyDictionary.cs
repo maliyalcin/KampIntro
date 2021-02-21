6f55ff236f8cd6688eca278bfd61b99f0d96d091
maliyalcin08 <maliyalcin08@gmail.com>
2021-02-21 18:15:18 +0300

    MyDictionary

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<TN, TA>
+    {
+        TN[] names;
+        TA[] ages;
+
+        public MyDictionary()
+        {
+            names = new TN[0];
+            ages = new TA[0];
+        }
+
+        public void Add(TN name, TA age)
+        {
+            TN[] tempArrayName = names;
+            TA[] tempArrayAge = ages;
+
+            names = new TN[tempArrayName.Length + 1];
+
+            for (int i = 0; i < tempArrayName.Length; i++)
+            {
+                names[i] = tempArrayName[i];
+            }
+
+            names[names.Length - 1] = name;
+
+            ages = new TA[tempArrayAge.Length + 1];
+
+            for (int i = 0; i < tempArrayAge.Length; i++)
+            {
+                ages[i] = tempArrayAge[i];
+            }
+
+            ages[ages.Length - 1] = age;
+
+            Console.WriteLine("Name : " + name + " \n " + "Age : " + age);
+
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string,int> myDictionary = new MyDictionary<string, int>();
+            myDictionary.Add("Ali",23);
+            myDictionary.Add("Fatih", 20);
+            myDictionary.Add("Yasin",25);
+        }
+    }
+}
